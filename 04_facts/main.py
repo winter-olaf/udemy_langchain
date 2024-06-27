027b0d5803aadf8b702bdc8d0bcbc125c021cef4
zoonun <engcraft1102@gmail.com>
2024-06-27 13:18:15 +0900

    adjust file tree

--- 04_facts/main.py
+++ 04_facts/main.py
@@ -2,19 +2,17 @@ from dotenv import load_dotenv
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores.chroma import Chroma
 import os
 
 # load .env
 load_dotenv()
 API_KEY = os.environ.get('API_KEY')
 
+
 embeddings = OpenAIEmbeddings(
     openai_api_key=API_KEY,
 )
-
-emb = embeddings.embed_query("hi there")
-
-print(emb)
 
 text_splitter = CharacterTextSplitter(
     separator="\n",
@@ -26,6 +24,18 @@ loader = TextLoader("facts.txt")
 docs = loader.load_and_split(
     text_splitter=text_splitter,
 )
+
+db = Chroma.from_documents(
+    docs,
+    embedding=embeddings,
+    persist_directory="emb"
+)
+
+results = db.similarity_search_with_score("What is an interesting fact about the english language?", k=2)
+
+for result in results:
+    print(f"{result}\n")
+    # print(f"{result[0].page_content}\n")
 
 # for doc in docs:
 #     print(f"{doc.page_content}\n")
---
+++ 04_facts/prompt.py
@@ -0,0 +1,31 @@
+from dotenv import load_dotenv
+import os
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores.chroma import Chroma
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+
+
+# load .env
+load_dotenv()
+API_KEY = os.environ.get('API_KEY')
+chat = ChatOpenAI(
+    openai_api_key=API_KEY)
+
+embeddings = OpenAIEmbeddings(
+    openai_api_key=API_KEY,
+)
+db = Chroma(
+    persist_directory="emb",
+    embedding_function=embeddings
+)
+retriever = db.as_retriever()
+
+chain = RetrievalQA.from_chain_type(
+    llm=chat,
+    retriever=retriever,
+    chain_type="stuff"
+)
+
+result = chain.run("what is an interesting fact about the english language?")
+print(result)
